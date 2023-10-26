7defc11ccfa557a0282c939b1117184e89b67341
Randy <o1984531@gmail.com>
2023-10-27 00:37:51 +0800

    feat: 新增取得臺北市立動物園_館區簡介API宣告

--- ZoomInfo/app/src/main/java/com/example/zoominfo/api/interfaces/IApi.kt
+++ ZoomInfo/app/src/main/java/com/example/zoominfo/api/interfaces/IApi.kt
@@ -1,4 +1,12 @@
 package com.example.zoominfo.api.interfaces
 
+import com.example.zoominfo.api.response.ZoomCategoryInfo
+import kotlinx.coroutines.Deferred
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface IApi {
+
+    @GET("/api/v1/dataset/5a0e5fbb-72f8-41c6-908e-2fb25eff9b8a?scope=resourceAquire")
+    suspend fun fetchCategoryInfo(): Response<ZoomCategoryInfo>
 }
