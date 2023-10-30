c96142b84f461f2c18914218f5a21cd3b86075a0
Randy <o1984531@gmail.com>
2023-10-30 22:31:30 +0800

    feat: 新增  場館介紹頁面 收到新資料通知ui更新的流程

--- ZoomInfo/app/src/main/java/com/example/zoominfo/flow/zoom_summary/view/ZoomSummaryFragment.kt
+++ ZoomInfo/app/src/main/java/com/example/zoominfo/flow/zoom_summary/view/ZoomSummaryFragment.kt
@@ -7,26 +7,34 @@ import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.GONE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.bumptech.glide.Glide
 import com.example.zoominfo.R
+import com.example.zoominfo.api.response.ZoomInfo
 import com.example.zoominfo.databinding.FragmentZoomSummaryBinding
+import com.example.zoominfo.flow.zoom_summary.viewmodel.ZoomSummaryViewModel
 import dagger.hilt.android.AndroidEntryPoint
 import java.lang.Exception
 
 @AndroidEntryPoint
-class ZoomSummaryFragment : Fragment() {
 
     // Arguments
     private val args: ZoomSummaryFragmentArgs by navArgs()
+    // ViewModel
+    private val mViewModel: ZoomSummaryViewModel by viewModels()
     // Binding
     private val mBinding: FragmentZoomSummaryBinding by lazy {
         FragmentZoomSummaryBinding.inflate(layoutInflater)
     }
+    // Adapter
+    private lateinit var mAdapter: ZoomInfoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -44,11 +52,19 @@ class ZoomSummaryFragment : Fragment() {
     }
 
     private fun initView() {
+        mAdapter = ZoomInfoAdapter(requireContext(), this)
+
         mBinding.apply {
             tvToolbarTitle.text = args.eName
             tvInfo.text = args.eInfo
             tvMemo.text = args.eMemo.ifBlank { getString(R.string.no_memo_info) }
             tvCategory.text = args.eCategory
+
+
+            rvZoomList.apply {
+                layoutManager = LinearLayoutManager(requireContext())
+                adapter = this@ZoomSummaryFragment.mAdapter
+            }
 
             Glide.with(requireContext())
                 .load(args.ePicUrl)
@@ -69,11 +85,19 @@ class ZoomSummaryFragment : Fragment() {
     }
 
     private fun initObserver() {
-        // TODO: Not yet implemented
+        mViewModel.filteredZoomInfoList.observe(viewLifecycleOwner) {filteredZoomInfoList ->
+            filteredZoomInfoList?.let {
+                mBinding.pbProgress.visibility = GONE
+                mAdapter.submitList(filteredZoomInfoList)
+            }
+        }
     }
 
     private fun initData() {
-        // TODO: Not yet implemented
-        Log.d("", "")
+        mBinding.pbProgress.visibility = VISIBLE
+
+        mViewModel.fetchZoomListByLocation(args.eName)
+    }
+
     }
 }
